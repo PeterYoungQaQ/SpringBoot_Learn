05b040265a15c6199e4d7241072813660b28c127
uestc_yhr <961154789@qq.com>
2021-01-25 22:18:06 +0800

    update

---
+++ demo/src/main/java/com/example/demo/intercepter/LoginInterceptor.java
@@ -0,0 +1,38 @@
+package com.example.demo.intercepter;
+/*
+ * @Author: Haoran
+ * @Date: 2021/1/25 22:00
+ * @Description:
+ */
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        System.out.println("LoginInterceptor preHandle =====");
+
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+        System.out.println("LoginInterceptor postHandle =====");
+
+        HandlerInterceptor.super.postHandle(request, response, handler,modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+        System.out.println("LoginInterceptor afterCompletion =====");
+
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
