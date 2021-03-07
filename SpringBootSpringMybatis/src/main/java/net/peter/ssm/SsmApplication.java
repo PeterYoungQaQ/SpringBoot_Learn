a8f98823081c68a0531e887ed38393e35d7ad688
uestc_yhr <961154789@qq.com>
2021-03-07 09:17:28 +0800

    update

--- SpringBootSpringMybatis/src/main/java/net/peter/ssm/SsmApplication.java
+++ SpringBootSpringMybatis/src/main/java/net/peter/ssm/SsmApplication.java
@@ -3,6 +3,9 @@ package net.peter.ssm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Administrator
+ */
 @SpringBootApplication
 public class SsmApplication {
 
