4f881abddd7222781fae07dff8efeef6f4676415
uestc_yhr <961154789@qq.com>
2021-03-12 09:23:36 +0800

    update

---
+++ SpringBootSpringMybatis/src/main/java/net/peter/ssm/config/InterceptorConfig.java
@@ -0,0 +1,35 @@
+package net.peter.ssm.config;
+/*
+ * @Author: Haoran
+ * @Date: 2021/3/12 8:59
+ * @Description: 拦截器配置类
+ */
+
+import net.peter.ssm.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 拦截器配置
+ *
+ * 不用权限可以访问url： api/v1/pub/
+ * 需要权限访问的url： api/v1/pri/
+ */
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Bean
+    LoginInterceptor loginInterceptor(){
+        return new LoginInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //拦截全部
+        registry.addInterceptor(loginInterceptor()).addPathPatterns("/api/v1/pri/*/*/**")
+        //不不拦截哪些路路径 斜杠⼀一定要加
+                .excludePathPatterns("/api/v1/pri/user/login","/api/v1/pri/user/register");
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+}
--- SpringBootSpringMybatis/src/main/java/net/peter/ssm/interceptor/LoginInterceptor.java
+++ SpringBootSpringMybatis/src/main/java/net/peter/ssm/interceptor/LoginInterceptor.java
@@ -11,7 +11,6 @@ import net.peter.ssm.utils.JWTUtils;
 import net.peter.ssm.utils.JsonData;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,33 +27,29 @@ public class LoginInterceptor implements HandlerInterceptor {
      * @throws Exception
      */
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) throws Exception {
         try {
             String accessToken = request.getHeader("token");
-            if (accessToken == null){
-                request.getParameter("token");
+            if (accessToken == null) {
+                accessToken = request.getParameter("token");
             }
-
-            // 如果token不为空的情况下
-            if (StringUtils.isNotBlank(accessToken)){
+            if (StringUtils.isNotBlank(accessToken)) {
                 Claims claims = JWTUtils.checkJWT(accessToken);
-                if (claims == null){
-                    // 如果为空，告诉登入失败，重新登入
-                    sendJsonMessage(response, JsonData.buildError("登入过期，重新登入"));
+                if (claims == null) {
+                    //告诉登录过期，重新登录
+                    sendJsonMessage(response, JsonData.buildError("登录过期，重新登录"));
                     return false;
                 }
-                Integer userId = (Integer) claims.get("id");
-                String userName = (String) claims.get("name");
-                String userPhone = (String) claims.get("phone");
-
-                request.setAttribute("user_id", userId);
-                request.setAttribute("user_name", userName);
-                request.setAttribute("user_phone", userPhone);
-
+                Integer id = (Integer) claims.get("id");
+                String name = (String) claims.get("name");
+                request.setAttribute("user_id", id);
+                request.setAttribute("name", name);
                 return true;
             }
-        }catch (Exception ignored){}
+        }catch (Exception e){
+            sendJsonMessage(response, JsonData.buildError("登录过期，重新登录"));
+        }
 
         return false;
     }
@@ -62,15 +57,15 @@ public class LoginInterceptor implements HandlerInterceptor {
     /**
      * 响应json数据，将错误信息通过流信息传递给前端
      * @param response
-     * @param object
+     * @param obj
      */
-    public static void sendJsonMessage(HttpServletResponse response, Object object){
+    public static void sendJsonMessage(HttpServletResponse response, Object obj){
 
-        try {
+        try{
             ObjectMapper objectMapper = new ObjectMapper();
-            response.setContentType("application/json; charset=UTF-8");
-            PrintWriter writer = response.getWriter();
-            writer.println(objectMapper.writeValueAsString(object));
+            response.setContentType("application/json; charset=utf-8");
+                    PrintWriter writer = response.getWriter();
+            writer.print(objectMapper.writeValueAsString(obj));
             writer.close();
             response.flushBuffer();
         }catch (Exception e){
@@ -78,15 +73,4 @@ public class LoginInterceptor implements HandlerInterceptor {
         }
     }
 
-
-
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-
-    }
 }
